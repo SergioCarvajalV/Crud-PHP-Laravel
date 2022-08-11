587c21e9f9eadbd511ae1f674ee6945fa221fcf3
SergioCarvajalV <scarvajal@utem.cl>
2022-08-11 03:14:38 -0400

    laravel instalado y front crear nota listo

---
+++ resources/views/crearNota.blade.php
@@ -0,0 +1,21 @@
+@extends('app')
+
+@section('content')
+
+    <div class="container w-25 p-4 bg-light text-dark bg-opacity-25 " >
+        <form >
+            <div class="mb-3>
+                <label for="titulo" class="form-label">Titulo de la nota</label>
+                <input type="text" class="form-control" name="titulo" placeholder="Ingrese titulo de su nota">
+              </div>
+              <div class="mb-3">
+                <label for="Nota" class="form-label">Descripcion de su nota</label>
+                <textarea class="form-control" name="Nota" rows="5"></textarea>
+              </div>
+              <div>
+                <button type="submit" class="btn btn-primary" > Crear Nota </button>
+              </div>
+        </form>
+    </div>
+
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('content')
+
+    <div class="container w-25 border p-4">
+        <form action="">
+
+        </form>
+    </div>
+
+@endsection
